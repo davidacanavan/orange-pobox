ac9f8cd786154dd8207cf910df08933405b99840
davidacanavan <d.i.dave.canavan@gmail.com>
2012-11-24 17:48:48 +0000

    Loading bar in final activity

--- Mailbox Setup/src/com/orange/pobox/FinalActivity.java
+++ Mailbox Setup/src/com/orange/pobox/FinalActivity.java
@@ -16,20 +16,31 @@ public class FinalActivity extends Activity
      super.onCreate(savedInstanceState);
      this.requestWindowFeature(Window.FEATURE_NO_TITLE);
      this.setContentView(R.layout.final_activity);
-     this.progressDialog = ProgressDialog.show(this, "Sending Application", 
-    		 "Your application is currently being sent to the UPS ofice of your choice.");
      Intent creator = this.getIntent();
      StoreLocation storeLocation = (StoreLocation) creator.getSerializableExtra(INTENT_INPUT_STORE_LOCATION);
      CustomerInfo customerInfo = (CustomerInfo) creator.getSerializableExtra(INTENT_INPUT_CUSTOMER_INFO);
      this.createPDFAndSendEmail(storeLocation, customerInfo);
-     this.progressDialog.dismiss();
     }
     
     private void createPDFAndSendEmail(StoreLocation storeLocation, CustomerInfo customerInfo)
-    { // TODO This is you Monica
+    {
+     final ProgressDialog progressDialog = ProgressDialog.show(this, "Sending Application", 
+       		 "Your application is currently being sent to the UPS ofice of your choice.");
+     Thread thread = new Thread()
+     {
+    	 public void run()
+    	 {
+    		 try
+    		 {// TODO This is you Monica
+    		  Thread.sleep(10000);
+    		  progressDialog.dismiss();
+    		 }
+    		 catch (InterruptedException e) {}
+    	 }
+     };
+     thread.start();
     }
 
- private ProgressDialog progressDialog;
  public static final String INTENT_INPUT_STORE_LOCATION = "store_location";
  public static final String INTENT_INPUT_CUSTOMER_INFO = "customer_information";
 }
